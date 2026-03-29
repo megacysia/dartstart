f6228b2c162e48cf6af7792fd0ea0d1d1c41bfcb
megacysia <marcelina.nocon783.com>
2026-03-29 19:40:59 +0200

    dart

---
+++ cli/bin/cli.dart
@@ -0,0 +1,45 @@
+import 'dart:io'; // Add this line at the top
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+// ... (your existing main function)
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
+}
+
+
+// ... (your existing printUsage() function)
+
+
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
